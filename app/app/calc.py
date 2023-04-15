bbd8d7f5f40ee63c834e47938b2e9fbdadedd4be
Anjila Alvi <anjilaalvi@Anjilas-Air.lan>
2023-04-14 22:56:36 -0400

    Added GitHub Action.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator funtions
+"""
+
+def add(x,y):
+    """Add x and y return result."""
+    return x + y
+
+def subract(x,y):
+    """subtract x and y return result"""
+    return y - x  
--- app/app/test.py
+++ app/app/test.py
@@ -13,3 +13,10 @@ class calcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+
+    def test_subract_numbers(self):
+        """Test to subract numbers"""
+        res = calc.subract(10, 15)
+
+        self.assertEqual(res, 5)
